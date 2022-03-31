eb4a82f3b785b86ccbc4364451ab5d691397624e
NickBwalley <nickbwalley@gmail.com>
2022-03-31 10:49:39 +0300

    expense item component

--- expense-tracker-app/src/components/ExpenseItem.js
+++ expense-tracker-app/src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-    return <h2>ExpenseItem!</h2>
+  return (
+    <div>
+      <div>March 28, 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.99</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
