23bfebce046c4bf5c4c993dbefedf6e22be864cb
NickBwalley <nickbwalley@gmail.com>
2022-04-02 15:27:45 +0300

    using one state instead

--- expense-tracker-app/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker-app/src/components/NewExpense/ExpenseForm.js
@@ -3,20 +3,36 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [enterTitle, setEnteredTitle] = useState("");
-  const [enterAmount, setEnteredAmount] = useState("");
-  const [enterDate, setEnteredDate] = useState("");
-
+  //   const [enterTitle, setEnteredTitle] = useState("");
+  //   const [enterAmount, setEnteredAmount] = useState("");
+  //   const [enterDate, setEnteredDate] = useState("");
+  const [userInput, setUserInput] = useState({
+    enterTitle: "",
+    enterAmount: "",
+    enterDate: "",
+  });
   const titleChangeHandler = (event) => {
-    setEnteredTitle(event.target.value);
+    // setEnteredTitle(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
   };
 
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.target.value);
+    // setEnteredAmount(event.target.value);
+    setUserInput({
+      ...userInput,
+      enterAmount: event.target.value,
+    });
   };
 
   const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
+    // setEnteredDate(event.target.value);
+    setUserInput({
+      ...userInput,
+      enterDate: event.target.value,
+    });
   };
 
   return (
