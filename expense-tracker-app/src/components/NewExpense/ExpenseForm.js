ff971e7891b7be44422fcef7df3990020433fcb4
NickBwalley <nickbwalley@gmail.com>
2022-04-02 15:50:58 +0300

    handling form submissions

--- expense-tracker-app/src/components/NewExpense/ExpenseForm.js
+++ expense-tracker-app/src/components/NewExpense/ExpenseForm.js
@@ -3,9 +3,9 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [enterTitle, setEnteredTitle] = useState("");
-  const [enterAmount, setEnteredAmount] = useState("");
-  const [enterDate, setEnteredDate] = useState("");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
   //   const [userInput, setUserInput] = useState({
   //     enterTitle: "",
   //     enterAmount: "",
@@ -38,8 +38,20 @@ const ExpenseForm = () => {
     // });
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
