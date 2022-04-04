678c0e84fec171b359cf4c6ee1ecefea5be08272
NickBwalley <nickbwalley@gmail.com>
2022-04-04 08:25:01 +0300

    just few modifications made

--- expense-tracker-app/src/components/Expenses/ExpenseItem.js
+++ expense-tracker-app/src/components/Expenses/ExpenseItem.js
@@ -7,7 +7,7 @@ import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
   // function clickHandler(){}
   const [title, setTitle] = useState(props.title);
-  console.log("ExpenseItem evaluated by React");
+  // console.log("ExpenseItem evaluated by React");
 
   const clickHandler = () => {
     setTitle("Updated!");
@@ -18,7 +18,7 @@ const ExpenseItem = (props) => {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
